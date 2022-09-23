0571047f7bcfb8ff803327a5325c409e692d78c4
Mringa School <lorna.mikatei@student.moringaschool.com>
2022-09-23 10:34:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 =31
+const num2  =2
+const multiply=num1*num2
+const random=(num1)
+Math.random (num1);
+const num3= 32
+const num4= 7
+const mod=num3%num4
+let max=20
+Math.max=(1,3,4,5,20)
